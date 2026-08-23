b19eece22b6fcc8cade431d4464f11bf57c2287a
SchlattSupporter <schlatsuport@gmail.com>
2026-08-23 11:17:10 -0700

    Update BuildConfig.kt

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,13 +1,12 @@
 object BuildConfig {
     const val MINECRAFT_VERSION: String = "1.21.11"
-    const val FABRIC_LOADER_VERSION: String = "0.19.3"
+    const val FABRIC_LOADER_VERSION: String = "0.18.1"
     const val FABRIC_API_VERSION: String = "0.141.6+1.21.11"
     const val UKULIB_VERSION: String = "1.10.2+1.21.11"
 
-    const val NEOFORGE_VERSION: String = "YOUR_1.21.11_NEOFORGE_VERSION"
+    const val NEOFORGE_VERSION: String = "26.2.0.0-beta"
 
     const val MOD_VERSION: String = "2.6.0"
-
     const val MODRINTH_PROJECT_ID: String = "dpkYdLu5"
 
     fun createVersionString(): String {
